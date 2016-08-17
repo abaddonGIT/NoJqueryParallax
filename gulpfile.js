9d4f8709f97f5ed5cf0d31d7880d28df06ae2c97
abaddonGIT <abaddongit@gmail.com>
2016-08-17 23:00:35 +0300

    new plugin

--- gulpfile.js
+++ gulpfile.js
@@ -3,10 +3,3 @@
  */
 "use strict";
 var gulp = require('gulp');
-var w3c = require('./index-compiled.js');
-
-gulp.task('validate', function () {
-    gulp.src('./*.html')
-        .pipe(w3c())
-        .pipe(gulp.dest('./'));
-});
